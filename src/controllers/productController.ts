a73e2b9ac3b57cbc8a04511e5890f6057bbfacb2
armelle09 <alelarge@wecareatwork.com>
2023-07-06 21:07:11 +0200

    Add id key to static product list

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,12 +4,12 @@ async function productController(req: Request, res: Response) {
   return res
       .status(200)
       .json([
-        {imageUrl: "https://media.famillemary.fr/media/catalog/product/cache/ab108d34ae64fe8e0dffcb4c479290ce/m/i/miel_fleurs_anjou_1.png",title: 'Miel de fleurs', description: "Toutes les fleurs", price:"25 €", quantity: 4, stocked: true},
-        {imageUrl:"https://www.noel-apiculture.fr/wp-content/uploads/2019/06/Tilleul-500-g.jpg",title: 'Miel tilleul', description: "Du tilleul rien que pour vous", price:"10 €", quantity: 6, stocked: true},
-        {imageUrl:"https://www.miel-lerucherdelours.fr/1765-thickbox_default/miel-de-foret.jpg",title: 'Miel de foret', description: "De forêt pour plus d'onctuosité", price:"20 €", quantity: 10, stocked: true},
-        {imageUrl: "https://media.famillemary.fr/media/catalog/product/cache/ab108d34ae64fe8e0dffcb4c479290ce/m/i/miel_fleurs_anjou_1.png",title: 'Miel de fleurs', description: "Toutes les fleurs", price:"25 €", quantity: 4, stocked: true},
-        {imageUrl:"https://www.noel-apiculture.fr/wp-content/uploads/2019/06/Tilleul-500-g.jpg",title: 'Miel tilleul', description: "Du tilleul rien que pour vous", price:"10 €", quantity: 6, stocked: true},
-        {imageUrl:"https://www.miel-lerucherdelours.fr/1765-thickbox_default/miel-de-foret.jpg",title: 'Miel de foret', description: "De forêt pour plus d'onctuosité", price:"20 €", quantity: 10, stocked: true}
+        {id:1, quantity: 1, imageUrl: "https://media.famillemary.fr/media/catalog/product/cache/ab108d34ae64fe8e0dffcb4c479290ce/m/i/miel_fleurs_anjou_1.png",title: 'Miel de fleurs', description: "Toutes les fleurs", price:2},
+        {id:2, quantity: 1, imageUrl:"https://www.noel-apiculture.fr/wp-content/uploads/2019/06/Tilleul-500-g.jpg",title: 'Miel tilleul', description: "Du tilleul rien que pour vous", price:10},
+        {id:3, quantity: 1, imageUrl:"https://www.miel-lerucherdelours.fr/1765-thickbox_default/miel-de-foret.jpg",title: 'Miel de foret', description: "De forêt pour plus d'onctuosité", price:20},
+        {id:4, quantity: 1, imageUrl: "https://media.famillemary.fr/media/catalog/product/cache/ab108d34ae64fe8e0dffcb4c479290ce/m/i/miel_fleurs_anjou_1.png",title: 'Miel de fleurs', description: "Toutes les fleurs", price:25},
+        {id:5, quantity: 1, imageUrl:"https://www.noel-apiculture.fr/wp-content/uploads/2019/06/Tilleul-500-g.jpg",title: 'Miel tilleul', description: "Du tilleul rien que pour vous", price:10},
+        {id:6, quantity: 1, imageUrl:"https://www.miel-lerucherdelours.fr/1765-thickbox_default/miel-de-foret.jpg",title: 'Miel de foret', description: "De forêt pour plus d'onctuosité", price:20}
       ])
 };
 
